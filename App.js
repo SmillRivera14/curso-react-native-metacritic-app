e586557d496335c1e338707d8639aa3c22bcb378
SmillRivera14 <smillriveradiaz14@gmail.com>
2024-07-17 21:19:23 -0400

    safeAreacontext

--- App.js
+++ App.js
@@ -1,39 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  // Button,
-  TouchableHighlight,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Main } from "./components/Main";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{ color: "white" }}>Aquí hay una app</Text>
-
-      <StatusBar style="light" />
-
-      {/* <Button
-        title="Pulsa aquí"
-        onPress={() => alert('Hola')}
-      /> */}
-
-      <TouchableHighlight
-        underlayColor={"#09f"}
-        onPress={() => alert("Hola")}
-        style={{
-          width: 200,
-          height: 200,
-          backgroundColor: "yellow",
-          borderRadius: 100,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text style={{ color: "black" }}>Pulsar aquí</Text>
-      </TouchableHighlight>
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -43,5 +20,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
+    paddingHorizontal: 12,
   },
 });
