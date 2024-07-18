83dbc0a67354ed52d3ec606ff4b8c59cd6b46efa
SmillRivera14 <smillriveradiaz14@gmail.com>
2024-07-17 21:32:12 -0400

    Extraer lista en un componente

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import { View, ScrollView } from "react-native";
 import { getLatestGames } from "../lib/metacritic";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { GameCard } from "./GameCard";
 
 export function Main() {
   const [games, setGames] = useState([]);
@@ -18,41 +19,9 @@ export function Main() {
       {/* primera forma de hacer que sea deslizable  */}
       <ScrollView>
         {games.map((game) => (
-          <View key={game.slug} style={styles.card}>
-            <Image source={{ uri: game.image }} style={styles.image} />
-            <Text style={styles.title}>{game.title}</Text>
-            <Text style={styles.description}>{game.description}</Text>
-            <Text style={styles.score}>{game.score}</Text>
-          </View>
+          <GameCard key={game.slag} game={game} />
         ))}
       </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  card: {
-    marginBottom: 42,
-  },
-  image: {
-    width: 107,
-    height: 147,
-    borderRadius: 10,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginTop: 10,
-    color: "#fff",
-  },
-  description: {
-    fontSize: 16,
-    color: "#eee",
-  },
-  score: {
-    fontSize: 22,
-    fontWeight: "bold",
-    color: "green",
-    marginBottom: 10,
-  },
-});
