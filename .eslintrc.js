11e5e1d7e78496541c434760f3dca0f3e955036c
SmillRivera14 <smillriveradiaz14@gmail.com>
2024-07-17 20:05:07 -0400

    eslint, prettier, etc

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// .eslintrc.js
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "lf" }],
+  },
+};
--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  // Button,
+  TouchableHighlight,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: "white" }}>Aquí hay una app</Text>
+
+      <StatusBar style="light" />
+
+      {/* <Button
+        title="Pulsa aquí"
+        onPress={() => alert('Hola')}
+      /> */}
+
+      <TouchableHighlight
+        underlayColor={"#09f"}
+        onPress={() => alert("Hola")}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "yellow",
+          borderRadius: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "black" }}>Pulsar aquí</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -13,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
