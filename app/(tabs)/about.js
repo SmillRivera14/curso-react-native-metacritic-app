efb16484c198214359f8a23b5d2c3a576fbc22f1
SmillRivera14 <smillriveradiaz14@gmail.com>
2024-08-02 16:24:51 -0400

    Eliminación de iconos

--- app/(tabs)/about.js
+++ app/(tabs)/about.js
@@ -1,4 +1,4 @@
-import { Link } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { Pressable, ScrollView, Text, View } from "react-native";
 import { HomeIcon } from "../../components/Icons";
 import { Screen } from "../../components/Screen";
@@ -7,19 +7,8 @@ export default function About() {
   return (
     <Screen>
       <ScrollView>
-        <View className="flex-row items-center mb-4 ">
-          <Link asChild href="/">
-            <Pressable>
-              {({ pressed }) => (
-                <HomeIcon style={{ opacity: pressed ? 0.5 : 1 }} />
-              )}
-            </Pressable>
-          </Link>
-
-          <Text className="pl-2 text-2xl font-bold text-white">
-            Sobre el proyecto
-          </Text>
-        </View>
+        {/* <View className="flex-row items-center mb-4 ">
+        </View> */}
 
         <Text className="mb-4 text-white text-white/90">
           Claro, aquí tienes un texto Lorem Ipsum repetido 4 veces: Lorem ipsum
--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,6 @@
-import { Pressable, View } from "react-native";
-import { Link, Stack } from "expo-router";
+import { View } from "react-native";
+import { Stack } from "expo-router";
 import { Logo } from "../components/Logo";
-import { CircleInfoICon } from "../components/Icons";
 
 export default function Layaut() {
   return (
@@ -12,13 +11,6 @@ export default function Layaut() {
           headerTintColor: "white",
           headerTitle: "",
           headerLeft: () => <Logo />,
-          headerRight: () => (
-            <Link asChild href="/about">
-              <Pressable>
-                <CircleInfoICon />
-              </Pressable>
-            </Link>
-          ),
         }}
       />
     </View>
